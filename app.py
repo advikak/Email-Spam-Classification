e7179ae0769d3a605a5f751147f6727e9e13a333
advikak <advika.kumari29@gmail.com>
2024-04-29 15:01:15 -0700

    Project

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()  # lowercase
+    text = nltk.word_tokenize(text)  # tokenize
+
+    y = []
+
+    for i in text:
+        if i.isalnum():
+            if i not in stopwords.words('english') and i not in string.punctuation:
+                y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("Spam Classifier")
+
+input_sms = st.text_area("Enter message")
+
+if st.button('Predict'):
+    transformed_sms = transform_text(input_sms)
+    vector_input = tfidf.transform([transformed_sms])
+    result = model.predict(vector_input)[0]
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
